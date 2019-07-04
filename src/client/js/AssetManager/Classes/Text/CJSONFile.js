095804ac06326c75fecdce9f9f7bd5bd621eb4da
SausageManZ <42648988+SausageManZ@users.noreply.github.com>
2019-07-04 12:42:36 +0200

    Update CJSONFile.js

--- src/client/js/AssetManager/Classes/Text/CJSONFile.js
+++ src/client/js/AssetManager/Classes/Text/CJSONFile.js
@@ -12,7 +12,7 @@ export default class JSONFile {
                 _this._fileContent
             );
             downloadCallback(_this._objectContent);
-        })
+        });
     }
 
     get() {
