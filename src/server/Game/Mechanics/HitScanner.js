048be06aac07e6fd33ccffe6166bf01c95dce2f8
SausageManZ <arostem@gmail.com>
2019-07-07 21:10:28 +0200

    Update HitScanner.js

--- src/server/Game/Mechanics/HitScanner.js
+++ src/server/Game/Mechanics/HitScanner.js
@@ -104,9 +104,7 @@ class HitScanner {
                 }
 
                 if (Vector2D.intersect(a, b, e.topLeft, e.bottomLeft)) {
-                    if (this._stopAtEntity) {
-                    }
-                    b.set(Vector2D.getIntersectedPos(a, b, e.topLeft, e.bottomLeft));
+                    if (this._stopAtEntity) b.set(Vector2D.getIntersectedPos(a, b, e.topLeft, e.bottomLeft));
                     this.onEntityHit(e, entityManager);
                 }
 
