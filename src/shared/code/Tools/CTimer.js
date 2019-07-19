55b88c0a9c14245b52acf10ce17c89c851eb58a7
SausageManZ <42648988+SausageManZ@users.noreply.github.com>
2019-07-19 14:35:41 +0200

    timer

---
+++ src/shared/code/Tools/CTimer.js
@@ -0,0 +1,24 @@
+export default class Timer {
+    constructor(time, callback, loop = true) {
+        this._maxTime = time;
+        this._currentTime = 0;
+        this._callback = callback;
+        this._completed = false;
+        this._loop = loop;
+    }
+
+    tick(deltaTime) {
+        this._currentTime += deltaTime;
+        if (this._currentTime >= this._maxTime) {
+            if (!this._completed) {
+                this._callback();
+                this._currentTime = 0;
+                this._completed = !this._loop;
+            }
+        }
+    }
+
+    reset() {
+        this._completed = false;
+    }
+}
