f8dc4ed8e7d0efa6bd15ed372a085ca876d9b6a1
SausageManZ <arostem@gmail.com>
2019-07-20 19:29:30 +0200

    code cleaning and useful data structure

--- src/server/Game/TileBased/TileSpawnPositionList.js
+++ src/server/Game/TileBased/TileSpawnPositionList.js
@@ -1,7 +1,7 @@
 const Tile = require("./Tile.js");
 const LootCrate = require("../Entity/Loot/Boxes/LootCrate.js");
 
-class SpawnerLocation {
+class SpawnLocation {
     constructor(x, y, name, entityClass) {
         this.x = x;
         this.y = y;
@@ -19,37 +19,47 @@ class SpawnerLocation {
 }
 
 // Retrieve it by Tile ID,
-SpawnerLocation.ENTITY_SPAWN_IDS = {};
+SpawnLocation.ENTITY_SPAWN_IDS = {};
 
-SpawnerLocation.setSpawnerByID = (id, name, classType, isOnlyPositionalData = false) => {
-    SpawnerLocation.ENTITY_SPAWN_IDS[id] = [name, isOnlyPositionalData, (x, y) => new SpawnerLocation(x, y, name, classType)];
+SpawnLocation.setSpawnerByID = (id, name, classType, isOnlyPositionalData = false) => {
+    SpawnLocation.ENTITY_SPAWN_IDS[id] = [name, isOnlyPositionalData, (x, y) => new SpawnLocation(x, y, name, classType)];
 };
 
-SpawnerLocation.createSpawner = (tileID, x, y) => {
-    return SpawnerLocation.ENTITY_SPAWN_IDS[tileID][2](x, y);
+SpawnLocation.getSpawnerByID = (id) => {
+    return SpawnLocation.ENTITY_SPAWN_IDS[id];
 };
 
-//SpawnerLocation.setSpawnerByID(90, "LootCrate", LootCrate);
-SpawnerLocation.setSpawnerByID(105, "PlayerLobby", null, true);
+SpawnLocation.createSpawner = (tileID, x, y) => {
+    return SpawnLocation.ENTITY_SPAWN_IDS[tileID][2](x, y);
+};
+
+//SpawnLocation.setSpawnerByID(90, "LootCrate", LootCrate);
+SpawnLocation.setSpawnerByID(105, "PlayerLobby", null, true);
 
 // Compositor class for TileMap that stores positions
 // of certain entities to be spawned.
-class EntitySpawnerLocator {
+class TileSpawnPositionList {
     constructor(tileMap) {
         this._spawners = {};
         this.scanMap(tileMap);
     }
 
+    getSpawnPosition(id) {
+        return this._spawners[id];
+    }
+
+
+
     scanMap(tileMap) {
         for (var y = 0; y < tileMap.h; y++) {
             for (var x = 0; x < tileMap.w; x++) {
                 var tileID = tileMap.getID(x, y);
-                if (SpawnerLocation.ENTITY_SPAWN_IDS[tileID]) {
+                if (SpawnLocation.getSpawnerByID(tileID)) {
                     if (!this._spawners[tileID]) {
                         this._spawners[tileID] = [];
-                        this._spawners[tileID].push(SpawnerLocation.createSpawner(tileID, x * Tile.SIZE, y * Tile.SIZE));
+                        this._spawners[tileID].push(SpawnLocation.createSpawner(tileID, x * Tile.SIZE, y * Tile.SIZE));
                     } else {
-                        this._spawners[tileID].push(SpawnerLocation.createSpawner(tileID, x * Tile.SIZE, y * Tile.SIZE));
+                        this._spawners[tileID].push(SpawnLocation.createSpawner(tileID, x * Tile.SIZE, y * Tile.SIZE));
                     }
                 }
             }
@@ -63,7 +73,7 @@ class EntitySpawnerLocator {
     }
 
     spawnAllOfType(id, entityManager) {
-        if (!SpawnerLocation.ENTITY_SPAWN_IDS[id][1]) {
+        if (!SpawnLocation.ENTITY_SPAWN_IDS[id][1]) {
             for (let spawner of this._spawners[id]) {
                 spawner.spawnHere(entityManager);
             }
@@ -75,4 +85,4 @@ class EntitySpawnerLocator {
     }
 }
 
-module.exports = EntitySpawnerLocator;
+module.exports = TileSpawnPositionList;
---
+++ src/shared/code/DataStructures/CObjectNotationMap.js
@@ -0,0 +1,60 @@
+// Better version of the stock JS Map.
+// Iteration of this object is way more
+// for performance and easier to use.
+
+export default class ObjectNotationMap {
+    constructor(allocation = Infinity) {
+        this._jsonContainer = {};
+        this._arrayContainer = [];
+        this._count = 0;
+        this._limit = allocation;
+    }
+
+    set(key, item) {
+        if (this._count < this._limit) {
+            this._jsonContainer[key] = item;
+            this._arrayContainer.push(item);
+            this._count++;
+        }
+        return item;
+    }
+
+    get(key) {
+        return this._jsonContainer[key];
+    }
+
+    remove(key) {
+        this._count--;
+        this._arrayContainer.splice(this._arrayContainer.indexOf(this._jsonContainer[key]));
+        delete this._jsonContainer[key];
+    }
+
+    indexOf(key) {
+        return this._arrayContainer.indexOf(this._jsonContainer[key]);
+    }
+
+    get length() {
+        return this._count;
+    }
+
+    get array() {
+        return this._arrayContainer;
+    }
+
+    get object() {
+        return this._jsonContainer;
+    }
+
+    clear() {
+        delete this["_jsonContainer"];
+        this._count = 0;
+        this._jsonContainer = {};
+        this._arrayContainer.clear();
+    }
+
+    forEach(callback) {
+        for (var key in this._jsonContainer) {
+            callback(key, this._jsonContainer[key]);
+        }
+    }
+}
