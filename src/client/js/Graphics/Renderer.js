56ff336f7842e46ab7271614768b88c7198bcb6b
SausageManZ <arostem@gmail.com>
2019-07-24 13:43:28 +0200

    smoothing out position of rect

--- src/client/js/Graphics/Renderer.js
+++ src/client/js/Graphics/Renderer.js
@@ -131,8 +131,8 @@ const R = {
         R.context.save();
         R.context.fillStyle = color;
         R.context.fillRect(
-            (x + (useCamera ? R.camera.boundPos.x : 0) | 0),
-            (y + (useCamera ? R.camera.boundPos.y : 0) | 0),
+            Math.round(x + (useCamera ? R.camera.boundPos.x : 0)),
+            Math.round(y + (useCamera ? R.camera.boundPos.y : 0)),
             width, height
         );
         R.context.restore();
