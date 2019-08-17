55b12b6804fe4493749b20f7ee18f9c23ed60f5d
SausageManZ <arostem@gmail.com>
2019-08-18 00:18:48 +0200

    Update UserPlayer.js

--- src/client/js/Game/Entity/Player/UserPlayer.js
+++ src/client/js/Game/Entity/Player/UserPlayer.js
@@ -9,7 +9,6 @@ const TILE_SIZE = 8;
 export default class UserPlayer extends RemotePlayer {
     constructor(data) {
         super(data);
-        this._serverState = data;
         this._localVel = new Vector2D(0, 0);
         this._localPos = new Vector2D(data._pos._x, data._pos._y);
         this._localSides = {
@@ -24,12 +23,9 @@ export default class UserPlayer extends RemotePlayer {
     }
 
     updateFromDataPack(dataPack, client) {
-        super.updateFromDataPack(dataPack, client);
-        this.serverReconciliation(client);
-        /*
-        this._serverState = Object.create(dataPack);
+        //super.updateFromDataPack(dataPack, client);
         this._output = dataPack;
-         */
+        this.serverReconciliation(client);
     }
 
     overlapTile(pos, e) {
@@ -37,26 +33,6 @@ export default class UserPlayer extends RemotePlayer {
             && pos._y < (e.y + TILE_SIZE)
             && pos._x + this._width > e.x
             && pos._x < (e.x + TILE_SIZE);
-    }
-
-    t_drawGhost() {
-        R.ctx.save();
-        R.ctx.globalAlpha = 0.4;
-        //this.animations.animate(RemotePlayer.sprite, this._serverState._teamName, 16, 16);
-        SpriteSheet.beginChanges();
-        if (this._serverState._movementState.direction === "left") {
-            RemotePlayer.sprite.flipX();
-        }
-        RemotePlayer.sprite.drawAnimated(
-            Math.round(this._serverState._pos._x) + R.camera.boundPos.x,
-            Math.round(this._serverState._pos._y) + R.camera.boundPos.y);
-        SpriteSheet.end();
-        R.ctx.restore();
-    }
-
-    draw() {
-        super.draw();
-        this.t_drawGhost();
     }
 
     update(deltaTime, client, currentMap) {
@@ -172,18 +148,17 @@ export default class UserPlayer extends RemotePlayer {
                 pending.splice(j, 1);
             } else {
                 // TODO: SCALABILITY
-                this._oldPos = this._output._pos;
                 this._localVel.x = 0;
 
                 if (input.keyStates[68]) {
                     if (!this._localSides.right) {
-                        this._localVel.x = 1;
+                        this._localVel.x = 60 * input.pressTime;
                     }
                 }
 
                 if (input.keyStates[65]) {
                     if (!this._localSides.left) {
-                        this._localVel.x = -1;
+                        this._localVel.x = -60 * input.pressTime;
                     }
                 }
 
