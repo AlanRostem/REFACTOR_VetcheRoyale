f439787256ddc9dd954bbb449be8d0ff265e195f
SausageManZ <arostem@gmail.com>
2019-08-23 23:57:21 +0200

    merge

--- src/server/Game/Entity/Loot/Weapons/Base/Firerer.js
+++ src/server/Game/Entity/Loot/Weapons/Base/Firerer.js
@@ -1,25 +1,29 @@
 const ONMap = require("../../../../../../shared/code/DataStructures/SObjectNotationMap.js");
 
 class Firerer {
-<<<<<<< HEAD
-    constructor(chargeTime = 0) {
+    constructor(chargeTime = 0, burstCount = 0) {
         this._chargeTime = 0;
         this._maxChargeTime = chargeTime;
         this._modes = {
             "charge": false,
             "burst": false,
         }
-=======
-    constructor() {
-
->>>>>>> parent of bb8ebe0... teamdata and firerer
     }
 
     update(weapon, player, entityManager, deltaTime) {
-        this.autoFireMode(weapon, player, entityManager, deltaTime);
+        this.firingUpdate(weapon, player, entityManager, deltaTime);
     }
 
-    autoFireMode(weapon, player, entityManager, deltaTime) {
+    doFire(weapon, player, entityManager, deltaTime) {
+        if (weapon._currentAmmo >= weapon._ammoPerShot) {
+            weapon.fire(player, entityManager, deltaTime);
+            weapon._currentAmmo -= weapon._ammoPerShot;
+        } else if (player.inventory.ammo > 0) {
+            weapon.activateReloadAction();
+        }
+    }
+
+    firingUpdate(weapon, player, entityManager, deltaTime) {
         // Can spam the mouse but the weapon always fires at the
         // same designated rate of fire.
         if (weapon._currentFireTime > 0) {
@@ -31,33 +35,25 @@ class Firerer {
         if (player.input.mouseHeldDown(1)) {
             if (weapon._currentFireTime <= 0 && !weapon._reloading) {
                 weapon._currentFireTime = (1 / deltaTime) / weapon._fireRate;
-<<<<<<< HEAD
-
-                if (this._chargeTime > 0) {
-                } else {
-                    if (weapon._currentAmmo >= weapon._ammoPerShot) {
-                        weapon.fire(player, entityManager, deltaTime);
-                        weapon._currentAmmo -= weapon._ammoPerShot;
-                    } else if (player.inventory.ammo > 0) {
-                        weapon.activateReloadAction();
+                if (this._maxChargeTime > 0) { // Check if we have charge mode on
+                    if (this._chargeTime > 0) {
+                        this._chargeTime -= deltaTime;
+                    } else {
+                        this._chargeTime = this._maxChargeTime;
+                        this.doFire(weapon, player, entityManager, deltaTime);
                     }
-=======
-                if (weapon._currentAmmo >= weapon._ammoPerShot) {
-                    weapon.fire(player, entityManager, deltaTime);
-                    weapon._currentAmmo -= weapon._ammoPerShot;
-                } else if (player.inventory.ammo > 0) {
-                    weapon.activateReloadAction();
->>>>>>> parent of bb8ebe0... teamdata and firerer
+                } else {
+                    this.doFire(weapon, player, entityManager, deltaTime);
+                }
+            }
+        } else {
+            if (this._maxChargeTime > 0) {
+                if (this._chargeTime < ) {
                 }
             }
         }
     }
 
-    chargeFireMode(weapon, player, entityManager, deltaTime) {
-        if (player.input.mouseHeldDown(1)) {
-
-        }
-    }
 }
 
 
