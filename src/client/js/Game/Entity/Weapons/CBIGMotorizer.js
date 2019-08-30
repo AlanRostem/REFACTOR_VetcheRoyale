36046e36abaf7a629dfc988488a8460e20a61285
SausageManZ <arostem@gmail.com>
2019-08-30 14:00:27 +0200

    Update CBIGMotorizer.js

--- src/client/js/Game/Entity/Weapons/CBIGMotorizer.js
+++ src/client/js/Game/Entity/Weapons/CBIGMotorizer.js
@@ -3,6 +3,6 @@ import CWeapon from "./CWeapon.js";
 export default class CBIGMotorizer extends CWeapon {
     onFire(client, deltaTime) {
         super.onFire(client, deltaTime);
-        AssetManager.get("Weapons/motorizer_s.oggp");
+        //AssetManager.get("Weapons/motorizer_s.oggp");
     }
 }
