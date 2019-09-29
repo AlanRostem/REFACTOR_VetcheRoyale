c0207d4b41089ad979a94c9fc54e98a90c0819f0
SausageManZ <arostem@gmail.com>
2019-09-29 19:39:45 +0200

    input works but theres callstack errors on weapon pick up

--- src/server/Game/World/WorldManager.js
+++ src/server/Game/World/WorldManager.js
@@ -145,7 +145,7 @@ class WorldManager {
             });
             //console.log("Player", data.id, "spawned in", player.homeWorldID);
         });
-/*
+
         this.dataBridge.addClientResponseListener("removePlayer", data => {
             let player = this.playerList.get(data.id);
             this.gameWorlds.get(player.homeWorldID).removePlayer(player.id);
@@ -160,7 +160,7 @@ class WorldManager {
             }
         });
 
-         */
+
     }
 }
 
