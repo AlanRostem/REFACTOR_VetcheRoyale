dd1e3ff553b54ed817e0ad9d720702c215352909
SausageManZ <arostem@gmail.com>
2019-10-19 02:00:46 +0200

    fixed gun spawner bøg

--- src/server/Game/World/Matches/PlayGround/GunSpawner.js
+++ src/server/Game/World/Matches/PlayGround/GunSpawner.js
@@ -10,13 +10,29 @@ const CKER90 = require("../../../Entity/Loot/Weapons/CKER90.js");
 
 const START_TILE = 14 * 8; // 14 rows times 8 cols on tile-sheet
 
+class GunSpawnFunction {
+    constructor(_class) {
+        this._class = _class;
+        this._func = (x, y) => new this._class(x, y);
+    }
+
+    getType() {
+        return this._class;
+    }
+
+    apply(x, y) {
+        return this._func(x, y);
+    }
+}
+
 const GUN_LIST = {
-    1: (x, y) => new CKER90(x, y),
-    2: (x, y) => new SEW_9(x, y),
-    3: (x, y) => new KE_6H(x, y),
-    4: (x, y) => new Interlux(x, y),
-    5: (x, y) => new BIGMotorizer(x, y),
+    1: new GunSpawnFunction(CKER90),
+    2: new GunSpawnFunction(SEW_9),
+    3: new GunSpawnFunction(KE_6H),
+    4: new GunSpawnFunction(Interlux),
+    5: new GunSpawnFunction(BIGMotorizer),
 };
+
 
 class GunSpawner extends Entity {
     constructor(x, y, args) {
@@ -31,7 +47,7 @@ class GunSpawner extends Entity {
 
     onEntityCollision(entity, entityManager) {
         super.onEntityCollision(entity, entityManager);
-        if (entity instanceof WeaponItem) {
+        if (entity instanceof GUN_LIST[this.gunID].getType()) {
             this.shouldSpawn = false;
             this.currentTickTime = this.maxTickTime;
             entity.resetLifeTime(entityManager);
@@ -47,7 +63,7 @@ class GunSpawner extends Entity {
 
         if (this.shouldSpawn) {
             if (this.currentTickTime <= 0) {
-                game.spawnEntity(this.pos.x, this.pos.y, GUN_LIST[this.gunID](this.pos.x, this.pos.y));
+                game.spawnEntity(this.pos.x, this.pos.y, GUN_LIST[this.gunID].apply(this.pos.x, this.pos.y));
                 this.shouldSpawn = false;
             }
         }
