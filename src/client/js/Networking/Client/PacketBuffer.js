a87a44ae015eec2c28d198cb07d8f2ecf53c242d
SausageManZ <arostem@gmail.com>
2019-11-06 00:24:43 +0100

    satan cker

--- src/client/js/Networking/Client/PacketBuffer.js
+++ src/client/js/Networking/Client/PacketBuffer.js
@@ -100,7 +100,7 @@ class PacketBuffer {
 
 PacketBuffer.createPacket = function (packet, snapShot, oneTimeValues = []) {
     let data = Object.copy(packet, oneTimeValues);
-    if (!Object.isJSON(snapShot) || !data) return snapShot;
+    if (!Object.isJSON(snapShot) || data === undefined || data === null) return snapShot;
     if (Object.keys(snapShot).length === 0) return snapShot;
     for (let key of Object.keys(snapShot)) {
             //if(oneTimeValues.find((e)=>{ return e === key })) continue;
