ba4de65ea96b755e8a9c333bfa5e272a62412d58
TheOneZalo <Benjamin.whittaker1999@gmail.com>
2019-11-19 18:12:07 +0100

    Update HTMLCreator.js

--- src/admin/Client/HTMLCreator.js
+++ src/admin/Client/HTMLCreator.js
@@ -1,33 +1,32 @@
 import PacketBuffer from "../../client/js/Networking/Client/PacketBuffer.js";
 
-Object.isJSON = function (obj) {
+Object.isJSON = function(obj) {
     return obj !== undefined && obj !== null && !(typeof obj === "number" && isNaN(obj)) && typeof obj !== "string" && typeof obj !== "number" && typeof obj !== "boolean";
 };
 
 class HTMLCreator {
     constructor(monitor) {
         this.monitor = monitor;
-        this.jsondata = {};
         this.types = ["World", "Entity", "Player"];
         this.keys = [];
         this.currentTypeIndex = 0;
 
-
         this.monitor.addUpdateCallback((data) => {
-            if (Object.keys(data).length)
-                if (data.keys) {
+
+            if (Object.keys(data).length) {
+                if (data.keys)
                     this.addDocReady(() => {
                         this.createTable("container", "WorldTable",
                             Object.values(data.data), Object.values(data.keys),
                             (id) => {
                                 this.currentTypeIndex++;
-                                console.log(this.currentType, true)
+                                console.log(this.currentType, true);
                                 this.selectFromTable(id, this.currentType);
                             });
                     });
-                } else {
-                    this.updateTable("WorldTable", data.data)
-                }
+                else
+                    this.updateTable("WorldTable", data.data);
+            }
         });
     }
 
@@ -58,10 +57,24 @@ class HTMLCreator {
                 "data": json,
 
                 "columns": props.map(key => {
-                    return {"title": key, "data": key, "defaultContent": ""}
+                    return {
+                        "title": key,
+                        "data": key,
+                        "defaultContent": "",
+                        "render": function(data, type, row) {
+                            if (Object.isJSON(data)) {
+                                let value = "";
+                                Object.keys(data).forEach(prop => {
+                                    value += prop + ": " + data[prop] + " ";
+                                });
+                                return value;
+                            }
+                            return data;
+                        }
+                    }
                 }),
 
-                "createdRow": function (row, data, index) {
+                "createdRow": function(row, data, index) {
                     $(row).attr('id', data.id);
                     if (callback)
                         $(row).click(() => {
@@ -71,14 +84,8 @@ class HTMLCreator {
 
                 "columnDefs": [{
                     "targets": '_all',
-                    "createdCell": function (td, cellData, rowData, row, col) {
+                    "createdCell": function(td, cellData, rowData, row, col) {
                         $(td).attr('class', Object.keys(rowData)[col]);
-                        if (Object.isJSON(cellData)) {
-                            $(td).empty();
-                            $(td).append(Object.keys(cellData).map(key =>
-                                $("<span/>", {"class": key}).append(key + ": " + cellData[key] + " ")
-                            ))
-                        }
                     }
                 }]
             });
@@ -92,116 +99,27 @@ class HTMLCreator {
         let table = $("#" + name).DataTable();
 
         Object.keys(json).forEach(id => {
-            let row = table.row("#" + $.escapeSelector(id)).data();
-            if (row) {
+            let row = table.row("#" + $.escapeSelector(id));
+            let rowData = row.data();
+            if (rowData) {
                 Object.keys(json[id]).forEach(key => {
-                    row[key] = json[id][key]
+                    if (Object.isJSON(rowData[key]))
+                        Object.keys(json[id][key]).forEach(prop => {
+                            rowData[key][prop] = json[id][key][prop]
+                        });
+                    else
+                        rowData[key] = json[id][key]
                 });
-                //row.draw(false);
+                row.invalidate();
             } else {
                 table.row.add(
                     json[id]
-                ).draw(false);
+                );
             }
-
-            console.log(row);
+            table.draw(false);
         });
     }
 
-    /*
-    updateText(elm, json) {
-        if (!Object.isJSON(json)) return json;
-        return Object.keys(json).map(key => $("<p/>", {"class": key}).append(key + ":" + json[key]))
-    }
-
-
-    updateTable(parent, json, props, name, type, callback) {
-        if (!parent.children("." + $.escapeSelector(name)).length) {
-            parent.append($("<tr/>", {"class": "clickable-row " + name}).append(
-                props.map(key => $("<td/>", {"class": key}).append(this.updateText(json[key]))[0])
-            ));
-            if (callback)
-                this.addDocReady(() => {
-                    parent.children("." + $.escapeSelector(name)).click(() => {
-                        callback(name, type);
-                    });
-                });
-        } else {
-            props.forEach(key => {
-                if (json[key] !== undefined) {
-                    this.updateText(parent.children("." + $.escapeSelector(name)).children("." + $.escapeSelector(key)), json[key]);
-
-
-                            if (!Object.isJSON(json[key]))
-                                parent.children("." + $.escapeSelector(name)).children("." + $.escapeSelector(key)).contents().filter(function () {
-                                return this.nodeType === 3;
-                            }).first()[0].nodeValue = this.updateObject(json[key]);
-                        }else {
-
-                        }
-                }
-            })
-        }
-
-    };
-
-
-    createTable(parent, name, type, json, props, callback = undefined) {
-        parent = $("#" + parent);
-        if (!parent.children("#" + $.escapeSelector(name)).length) {
-            parent.append([
-                $("<table/>", {"id": name, "class": "table table-hover table-striped table-bordered "}).append([
-                    $("<thead/>").append($("<tr/>")).append(
-                        props.map(key => $("<th/>", {"class": "th-sm"}).append(key).css({"font-size": "20px"})[0])),
-                    $("<tbody/>")
-                ])
-            ])
-        }
-
-        Object.keys(json).forEach(key => {
-            this.updateTable(parent.children("table").children("tbody"), json[key], props, key, type, callback)
-        });
-
-    };
-
-    displayJson(json, name, root = $("<ul/>")) {
-        if (!root.is("ul")) {
-            let u = $("<ul id='" + name + "'/>");
-            root.append(u);
-            root = u;
-        }
-
-        if (!Object.isJSON(json))
-            return ($("<li/>", {"class": "list-group-item " + name}).append("<b>" + name + ": </b>" + json).appendTo(root));
-
-        let nextRoot = $("<ul/>", {"class": ""}).css("display", "none");
-
-        root.append([
-            $("<li/>", {"class": " list-group-item " + name}).append([
-                $("<span/>", {"class": "badge"}).css("cursor", "pointer"),
-                $("<div/>").css({"cursor": "pointer", "width": "100%"}).append("<b>" + name + "</b>"),
-                nextRoot.append(Object.keys(json).forEach((e) => {
-                        this.displayJson(json[e], e, nextRoot)
-                    })
-                )
-            ])
-        ]);
-
-        this.addDocReady(() => {
-            root.children("li").children(".badge").last().append(nextRoot.children("li").length);
-            root.children("li").children("div").last().click(function () {
-                $(this).siblings().toggle();
-            });
-        });
-
-        return root;
-    };
-
-
-
-    clearElement(elm) {
-        $("#" + elm).empty();
-    };*/
 
     addDocReady(callback) {
         $(document).ready(callback());
