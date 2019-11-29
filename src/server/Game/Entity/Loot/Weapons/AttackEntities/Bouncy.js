34a43d9805d8b410825f6d0d01b0b09e73bd0e1b
Alan Rostem <arostem@gmail.com>
2019-11-30 00:42:37 +0100

    optimized some projectile code

--- src/server/Game/Entity/Loot/Weapons/AttackEntities/Bouncy.js
+++ src/server/Game/Entity/Loot/Weapons/AttackEntities/Bouncy.js
@@ -1,6 +1,4 @@
 const Projectile = require("./Projectile.js");
-const TileCollider = require("../../../../TileBased/TileCollider.js");
-const Tile = require("../../../../TileBased/Tile.js");
 // A bouncy projectile. Requires to be manually removed in overridden class.
 class Bouncy extends Projectile {
     constructor(ownerID, x, y, w, h, angle, speed, arc, bounceFactor = 1) {
